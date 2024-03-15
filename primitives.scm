5f3451ce6b7117b2992e4a6daf644f0306826142
Lily Yang <lilyang@umich.edu>
2024-03-14 23:24:59 -0400

    primitive procedure w literal arguments done

--- primitives.scm
+++ primitives.scm
@@ -91,7 +91,7 @@
                 (environment (car args))
                 (evaluated-args (eval-args-with-env (cdr args) environment))
               )
-              (apply name evaluated-args) ; THIS LINE HAS ERROR
+              (apply native-impl evaluated-args) ; THIS LINE HAS ERROR
             )
           )
           (else
